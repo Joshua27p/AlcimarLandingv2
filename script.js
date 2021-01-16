8819d375ef68358f800b61f3577a2b609e846d64
Yoshua_Pequeño <josh27p=git config --global user.email josh27p@gmail.com>
2021-01-15 21:01:24 -0500

    metadatos SEO

--- script.js
+++ script.js
@@ -25,30 +25,31 @@
   const $form = d.querySelector(".contact-form"),
     $loader = d.querySelector(".contact-form-loader"),
     $response = d.querySelector(".contact-form-response");
-  $form.addEventListener("submit", (e)=>{
+  
+    $form.addEventListener("submit", (e) => {
     e.preventDefault();
     $loader.classList.remove("none");
     fetch("https://formsubmit.co/ajax/yoshuapequeno@gmail.com",{
       method:"POST",
-      body: new FormData(e.target)
+      body: new FormData(e.target),
     })
-    .then(res=> (res.ok? res.json() : Promise.reject(res)))
-    .then(json=>{
+    .then((res)=> (res.ok ? res.json() : Promise.reject(res)))
+    .then((json) => {
       console.log(json);
       location.hash = "#gracias";
       $form.reset();
     })
-    .catch(err=>{
+    .catch((err) => {
       console.log(err);
       let message=
-       err.statusText || "Ocurrio un error al enviar, intenta nuevamente"
+       err.statusText || "Ocurrio un error al enviar, intenta nuevamente";
       $response.querySelector(
         "h3"
-        ).innerHTML =`Error ${err.status}: ${message}`;
+        ).innerHTML = `Error ${err.status}: ${message}`;
     })
     .finally(()=>{
        $loader.classList.add("none");
-       setTimeout(()=>{
+       setTimeout(() => {
          location.hash= "#close";
        }, 3000);
     });
